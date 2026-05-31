dc7b918e69cf63b962700140db197a49494951b6
subasthican <manoharansubasthican@gmail.com>
2026-05-31 12:55:52 +0530

    Build add edit product page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import AdminDashboardPage from './pages/AdminDashboardPage.jsx';
 import AdminLoginPage from './pages/AdminLoginPage.jsx';
+import AddEditProductPage from './pages/AddEditProductPage.jsx';
 import ManageCategoriesPage from './pages/ManageCategoriesPage.jsx';
 import ManageOrdersPage from './pages/ManageOrdersPage.jsx';
 import ViewOrderDetailsPage from './pages/ViewOrderDetailsPage.jsx';
@@ -25,6 +26,7 @@ export default function App() {
       <Route path="/admin/categories" element={<ManageCategoriesPage />} />
       <Route path="/admin/orders" element={<ManageOrdersPage />} />
       <Route path="/admin/orders/:orderId" element={<ViewOrderDetailsPage />} />
+      <Route path="/admin/products/new" element={<AddEditProductPage />} />
     </Routes>
   );
 }
